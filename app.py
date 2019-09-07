ca879c5f41fce1512ac06c76c1774c003ef32305
nat121234 <51712319+nat121234@users.noreply.github.com>
2019-09-07 11:02:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def callback():
     json_line = json.dumps(json_line)
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
-    userText = decoded['events"][0]['message']['text']
+    userText = decoded["events"][0]['message']['text']
     #sendText(user,userText)
     if (userText == 'สวัสดี') :
         sendText(user,'สวัสดี คะ')
